e3d185851427b3db587aaa7c607b7a09f7e1804d
CoppaMan <dario.mp@bluewin.ch>
2017-04-22 18:02:37 +0200

    ChunkManager extended with custom save path Fixed small bug in chunk loading

--- Assets/Scripts/ChunkManager.cs
+++ Assets/Scripts/ChunkManager.cs
@@ -7,11 +7,18 @@ using System.IO;
 public class ChunkManager{
 
     private int size;
+    private string path;
 
-    public ChunkManager(int size) { this.size = size; }
+    public ChunkManager(int size, string path) {
+        this.size = size;
+        this.path = path;
+
+        if (!Directory.Exists(path)) {
+            Directory.CreateDirectory(path);
+        }
+    }
 
     public void SaveChunk(int x, int y, int z, byte[] chunks) {
-        //find file
 
         string xs = Math.Floor((double)x / (double)size).ToString();
         string ys = Math.Floor((double)y / (double)size).ToString();
@@ -24,12 +31,12 @@ public class ChunkManager{
         int oldLength;
         int newLength = chunks.Length;
 
-        if (!File.Exists(xs + ys + zs)) {
+        if (!File.Exists(path + "/" + xs + ys + zs)) {
             result = new byte[(size * size * size * 4) + newLength];
 
             space = 4 * pos;
         } else {
-            prev = File.ReadAllBytes(xs + ys + zs);
+            prev = File.ReadAllBytes(path + "/" + xs + ys + zs);
 
             for (int i = 0; i < pos; i++) {
                 space += 4 + BitConverter.ToInt32(prev, space);
@@ -57,31 +64,38 @@ public class ChunkManager{
             result[space + i + 4] = chunks[i];
         }
         
-        File.WriteAllBytes(xs + ys + zs, result);
+        File.WriteAllBytes(path + "/" + xs + ys + zs, result);
     }
 
     public byte[] LoadChunk(int x, int y, int z) {
         //find file
-        string xs = (x / size).ToString();
-        string ys = (x / size).ToString();
-        string zs = (x / size).ToString();
-        int pos = x % size + (size * (y % size)) + (size * size * (z % size));
+        string xs = Math.Floor((double)x / (double)size).ToString();
+        string ys = Math.Floor((double)y / (double)size).ToString();
+        string zs = Math.Floor((double)z / (double)size).ToString();
+        int chunkLenght;
+        int pos = ((x % size + size) % size) + (size * ((y % size + size) % size)) + (size * size * ((z % size + size) % size));
 
-        if (!File.Exists(xs + ys + zs)) return null; //Region does not exist
+        if (!File.Exists(path + "/" + xs + ys + zs)) {
+            Debug.LogError("No region file " + xs + " " + ys + " " + zs + " for this chunk");
+            return null;
+        } //Region does not exist
 
-        byte[] bytes = File.ReadAllBytes(xs + ys + zs);
+        byte[] bytes = File.ReadAllBytes(path + "/" + xs + ys + zs);
 
         int space = 0;
         for (int i = 0; i < pos; i++) {
             space += BitConverter.ToInt32(bytes, space) + 4;
         }
 
-        pos = BitConverter.ToInt32(bytes, space);
-        if (pos == 0) return null; // no chunk saved here
-
-        byte[] chunk = new byte[pos];
-        for(int i = 0; i < pos; i++) {
-            chunk[i] = bytes[space + i];
+        chunkLenght = BitConverter.ToInt32(bytes, space);
+        if (chunkLenght == 0) {
+            Debug.LogError("No chunk at position " + x.ToString() + " " + y.ToString() + " " + z.ToString());
+            return null;
+        } // no chunk saved here
+        
+        byte[] chunk = new byte[chunkLenght];
+        for(int i = 0; i < chunkLenght; i++) {
+            chunk[i] = bytes[space + 4 + i];
         }
 
         return chunk;
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class Test : MonoBehaviour {
 
@@ -8,6 +9,7 @@ public class Test : MonoBehaviour {
         Debug.Log(transform.localToWorldMatrix);
 
         //ChunkTest();
+        SaveChunk();
     }
 	
 	// Update is called once per frame
@@ -15,12 +17,52 @@ public class Test : MonoBehaviour {
 
     }
 
+    void SaveChunk() {
+        ChunkManager test = new ChunkManager(2, "chunks/for/me");
+        test.SaveChunk(0, -1, 0, new byte[] { 21, 53 });
+        test.LoadChunk(0, -1, 0);
+    }
+
     //Chunk saving and loading test
     void ChunkTest() {
-        ChunkManager test = new ChunkManager(2);
+        ChunkManager test = new ChunkManager(16, "Chunks");
+        int err = 0;
+        int cnt = 300;
+        Vector3[] co = new Vector3[cnt];
+        byte[][] chunks = new byte[cnt][];
+        byte[][] saved = new byte[cnt][];
 
-        test.SaveChunk(1, 0, 0, new byte[] { 2, 3, 5, 7 });
-        test.SaveChunk(0, 1, 0, new byte[] { 9, 11, 13, 17 });
-        test.SaveChunk(1, 1, 1, new byte[] { 6,6,6 });
+        for (int i = 0; i < cnt; i++) {
+            co[i] = Random.insideUnitSphere * 50;
+            int bytes = (int)Mathf.Ceil(Random.value * cnt);
+            chunks[i] = new byte[bytes];
+            for (int j = 0; j < bytes; j++) {
+                chunks[i][j] = (byte)(Random.value * 255);
+            }
+        }
+        for (int i = 0; i < cnt; i++) {
+            test.SaveChunk((int)co[i].x, (int)co[i].y, (int)co[i].z, chunks[i]);
+        }
+
+        for (int i = 0; i < cnt; i++) {
+            saved[i] = test.LoadChunk((int)co[i].x, (int)co[i].y, (int)co[i].z);
+        }
+
+        for(int i = 0; i < cnt; i++) {
+            if(!Enumerable.SequenceEqual(chunks[i], saved[i])) {
+
+                string a = "";
+                for(int j = 0; j < chunks[i].Length; j++) {
+                    a += chunks[i][j].ToString() + " ";
+                }
+                a += " expected, but got ";
+                for (int j = 0; j < chunks[i].Length; j++) {
+                    a += saved[i][j].ToString() + " ";
+                }
+                Debug.LogError(a);
+                err++;
+            }
+        }
+        Debug.Log("Load errors:" + err);
     }
 }
